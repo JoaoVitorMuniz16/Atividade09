a84ab232b3bbbeb2228834da8dc31f73b2d8628d
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-16 19:04:45 -0300

    Add files via upload

---
+++ Atividade09.py
@@ -0,0 +1,8 @@
+def maior(x,y):
+    if x > y:
+        print("O Numero,",x,"é maior do que o,",y,".")
+    else:
+        print("O Número,",y,", é maior que o número,",x,".")
+x = int(input("Digite o Primeiro número: "))
+y = int(input("Digite o Segundo número: "))
+maior(x,y)
